d06653180f6687cf2a8a4c74250c8815a5508982
jacapa146-spec <jacapa146@gmail.com>
2026-06-30 22:25:06 -0500

    Le agrege un h1 con un Hola mundo

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ function App() {
       <h1 class="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <h1 class="text-3xl font-bold underline">
+        Hola mundo
+      </h1>
 
     </>
   )
